bb92985104ad03eb56c075709667ea3ab6765242
Don Abney <tdonaldabney@gmail.com>
2014-04-29 14:20:12 -0400

    Added rest of test and associated prod code for Goon validation

--- src/main/groovy/GoonValidator.groovy
+++ src/main/groovy/GoonValidator.groovy
@@ -1,10 +1,25 @@
 class GoonValidator {
 
     void validate(Goon goon) throws GoonException {
-        checkNameFields(goon)
+        checkFirstName(goon)
+        checkLastName(goon)
+        checkBirthDate(goon)
+        checkPhoneNumber(goon)
     }
 
-    void checkNameFields(Goon goon) {
+    private void checkFirstName(Goon goon) {
         if(!goon.getFirstName()) throw new GoonException("No First Name")
+    }
+
+    private void checkLastName(Goon goon) {
+        if(!goon.getLastName()) throw new GoonException("No Last Name")
+    }
+
+    private void checkBirthDate(Goon goon) {
+        if(!goon.getBirthDate()) throw new GoonException("No Birth Date")
+    }
+
+    private void checkPhoneNumber(Goon goon) {
+        if(!goon.getPhoneNumber()) throw new GoonException("No Phone Number")
     }
 }
--- src/test/groovy/GoonValidatorTest.groovy
+++ src/test/groovy/GoonValidatorTest.groovy
@@ -1,12 +1,37 @@
 import org.junit.Test
+import org.junit.Before
 
 class GoonValidatorTest {
 
+    Goon goon
+    GoonValidator goonValidator = new GoonValidator()
+
+    @Before
+    void setUp() {
+        goon = new Goon([firstName: "John", birthDate: new Date(), addressOne: "123 Someplace", city: "Columbus", state: "Ohio", zipCode: 12345, phoneNumber: "614-555-1212"])
+    }
+
     @Test(expected = GoonException)
     void missingFirstNameThrowsGoonException() {
-        Goon goon = new Goon()
         goon.setFirstName("")
-        GoonValidator goonValidator = new GoonValidator()
+        goonValidator.validate(goon)
+    }
+
+    @Test(expected = GoonException)
+    void missingLastNameThrowsGoonException() {
+        goon.setLastName("")
+        goonValidator.validate(goon)
+    }
+
+    @Test(expected = GoonException)
+    void missingBirthDateThrowsGoonException() {
+        goon.setBirthDate(null)
+        goonValidator.validate(goon)
+    }
+
+    @Test(expected = GoonException)
+    void missingPhoneNumberThrowsGoonException() {
+        goon.setPhoneNumber("")
         goonValidator.validate(goon)
     }
 }
